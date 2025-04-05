5b0e92e06f317fa837610604be26df2e3d7ff5db
debbieAmus <debcakes4life@gmail.com>
2025-04-05 14:36:42 -0500

    updated

---
+++ Flaskapp/vars/setting.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbpostgre',
+        'USER': 'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'dbpostgre.c1ak8kw808nm.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+ 
